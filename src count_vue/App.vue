1d96a714ec8fa406d9030f61e2c11c4e2f45c9db
zhangjiewei <1525456439@qq.com>
2020-03-06 15:05:56 +0800

    vue版计数器

---
+++ src count_vue/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>次数{{count}},如果是偶数显示{{addEven}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementEven">increment if even</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      count:0
+    }
+  },
+  computed:{
+    addEven:function(){
+      return this.count % 2===0?'偶数':'奇数';
+    }
+  },
+  methods:{
+    increment:function(){
+      this.count++
+    },
+    decrement:function(){
+      this.count--;
+    },
+    incrementEven(){
+      if(this.count%2===0){
+        this.count++
+      }
+    },
+    incrementAsync(){
+      setTimeout(()=>{
+        this.count++;
+      },1000)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ 
+</style>
